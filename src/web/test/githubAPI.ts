ef4bd40650410709b0c248af1fe8c8cd86cb3155
Evotionce <47124903+Evotionce@users.noreply.github.com>
2023-07-14 06:57:04 +0200

    update failed scenario test

--- src/web/test/githubAPI.ts
+++ src/web/test/githubAPI.ts
@@ -1,6 +1,6 @@
 
 import { getFailedScenarios } from "../githubAPI";
 
-getFailedScenarios("poc/placeholder1", process.env.REPOSITORY_URL ?? '');
+getFailedScenarios("<insert actual branchName>", process.env.REPOSITORY_URL ?? '');
 
 
